0491183dc82bb8ee5204121fa8fc75c63865ba0f
plabonislam <shahnurislamplabon@gmail.com>
2025-02-16 12:53:54 +0600

    turbo log better visual with backend port address logging code

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  const server = await app.listen(process.env.PORT ?? 8000);
+  console.log("nest backend server is running on :", server.address().port+ " port")
 }
 bootstrap();
